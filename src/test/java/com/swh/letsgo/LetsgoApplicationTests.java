511caaedc4b4f5f96fed45aef43b40f029392d68
pabaep <sewon225@gmail.com>
2022-03-24 17:16:33 +0900

    Init: 프로젝트 초기 설정
    프로젝트 초기설정 완료

---
+++ src/test/java/com/swh/letsgo/LetsgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swh.letsgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
