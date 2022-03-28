43e0da565022b6144ee183119ba11ba863fe42bd
sewon <sewon225@gmail.com>
2022-03-28 13:39:18 +0900

    Feat: open api 컨트롤러, 서비스 기초 코드 작성
    open api 불러오기 위한 기초작업 수행
    (controller, service 기본틀 작성)

---
+++ src/main/java/com/swh/letsgo/service/OpenAPIServiceImpl.java
@@ -0,0 +1,11 @@
+package com.swh.letsgo.service;
+
+public class OpenAPIServiceImpl implements OpenAPIService{
+
+    @Override
+    public String callOpenAPI(String headerArea, String headerRegion) {
+        
+        return null;
+    }
+    
+}
