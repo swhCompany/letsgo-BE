7de9a2caee2253b417d983d51a6e30cb83229b05
sewon <sewon225@gmail.com>
2022-03-28 15:11:52 +0900

    Feat: apikey gitignore 설정
    apikey 두개 git ignore 설정 후 필요한 곳에 삽입

---
+++ src/main/java/com/swh/letsgo/config/ApiKey.java
@@ -0,0 +1,22 @@
+package com.swh.letsgo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiKey {
+    @Value("${library-api-key}")
+    private String libraryApiKey;
+
+    @Value("${museum-api-key}")
+    private String museumApiKey;
+
+    public String getLibraryApiKey() {
+        return libraryApiKey;
+    }
+
+    public String getMuseumApiKey() {
+        return museumApiKey;
+    }
+
+}
